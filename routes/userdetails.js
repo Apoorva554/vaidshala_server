5a57e98016fbaa98c76f490549ad573873b11175
Apoorvabk554 <apoorvabk@vaidshala.in>
2023-02-21 12:45:45 +0530

    added message in userdetails

--- routes/userdetails.js
+++ routes/userdetails.js
@@ -40,9 +40,56 @@ router.post("/", async (req, res) => {
   router.get("/:uid", async (req, res) => {
     try {
       const post = await Post.find({uid:req.params.uid});
-      res.json(post);
+
+        if (post.length >= 1) {
+          
+          res.json({
+            message: "User is present",
+            post
+          });
+          } else {
+            res.status(200).json({
+              message: "User not found",
+              post
+            });
+          }
+     
     } catch (err) {
       res.json({ message: "Not found" });
+    }
+  });
+
+  router.delete("/:uid", async (req, res) => {
+    try {
+      const removePost = await Post.deleteOne({ uid: req.params.uid });
+      res.json(removePost);
+    } catch (err) {
+      res.json({ message: err });
+    }
+  });
+  
+  // update post
+  router.patch("/:uid", async (req, res) => {
+    try {
+      const updatedPost = await Post.updateOne(
+        { uid: req.params.uid },
+        { $set: {
+            name: req.body.name,
+            mobile:req.body.mobile,
+            email:req.body.email,
+            dateofbirth:req.body.dateofbirth,
+            weight:req.body.weight,
+            height:req.body.height,
+            gender:req.body.gender,
+            payment:req.body.payment,
+            notificationToken:req.body.notificationToken,
+                
+
+        } }
+      );
+      res.json(updatedPost);
+    } catch (err) {
+      res.json({ message: err });
     }
   });
   
